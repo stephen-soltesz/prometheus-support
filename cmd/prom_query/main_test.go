42e1b4c53024d4d893be54f1dea2ba12d8d8c213
Stephen Soltesz <stephen.soltesz@gmail.com>
2017-05-17 23:11:18 -0400

    Simplify test logic.

--- cmd/prom_query/main_test.go
+++ cmd/prom_query/main_test.go
@@ -14,49 +14,23 @@
 package main_test
 
 import (
-	"fmt"
+	// "fmt"
 	"io/ioutil"
 	"path/filepath"
-	"strings"
+	// "strings"
 	"testing"
-	"time"
+	// "time"
 
-	"github.com/prometheus/common/model"
+	log "github.com/Sirupsen/logrus"
+	// "github.com/prometheus/common/model"
 
 	"github.com/prometheus/prometheus/promql"
 	//	"github.com/prometheus/prometheus/rules"
 )
 
-func TestAlertingRule(t *testing.T) {
-	suite, err := promql.NewTest(t, `
-		load 5m
-			http_requests{job="app-server", instance="0", group="canary"}	75 85  95 105 105  95  85
-			http_requests{job="app-server", instance="1", group="canary"}   80 90 100 110 120 130 140
-	`)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer suite.Close()
-
-	fmt.Printf("Running suite\n")
-	if err := suite.Run(); err != nil {
-		t.Fatal(err)
-	}
-
-	engine := suite.QueryEngine()
-	query, err := engine.NewInstantQuery(`http_requests{group="canary", job="app-server"} < 100`, model.Time(0).Add(10*time.Minute))
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	result := query.Exec(suite.Context())
-	if result.Err != nil {
-		t.Fatal(result.Err)
-	}
-
-	fmt.Printf("Type: %q\n", result.Value.Type())
-	actual := strings.Split(result.Value.String(), "\n")
-	fmt.Printf("Value: %q\n", actual)
+func init() {
+	// Only log the warning severity or above.
+	log.SetLevel(log.WarnLevel)
 }
 
 func TestEvaluations(t *testing.T) {
@@ -65,9 +39,11 @@ func TestEvaluations(t *testing.T) {
 		t.Fatal(err)
 	}
 	for _, fn := range files {
+		t.Logf("Running %s", fn)
 		content, err := ioutil.ReadFile(fn)
 		if err != nil {
-			t.Fatal(err)
+			t.Error(err)
+			continue
 		}
 		test, err := promql.NewTest(t, string(content))
 		if err != nil {

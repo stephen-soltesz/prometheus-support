259bad733df38dd744f900bc934bb5439f317e5b
Stephen Soltesz <stephen.soltesz@gmail.com>
2017-05-19 00:07:32 -0400

    Simplify

--- cmd/query_tester/main.go
+++ cmd/query_tester/main.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("basic go")
+	fmt.Println("Run:")
+	fmt.Println("go test -v github.com/m-lab/prometheus-support/cmd/query_tester -args -log.level warn")
 }

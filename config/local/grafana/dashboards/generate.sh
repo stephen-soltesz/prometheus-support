84074044ef7f845d2e6540653e7501e0071f5cc3
Stephen Soltesz <stephen.soltesz@gmail.com>
2017-12-03 21:56:45 -0500

    Add jsonnet build steps to generate.sh

--- config/local/grafana/dashboards/generate.sh
+++ config/local/grafana/dashboards/generate.sh
@@ -3,8 +3,15 @@
 BASEDIR=$( dirname "${BASH_SOURCE[0]}" )
 # Grafana requires world readership. Setting umask only affects the local shell.
 umask 0002
-for file in `ls ${BASEDIR}/*.dashboard.py` ; do
+for file in `ls ${BASEDIR}/*.dashboard.py` `ls ${BASEDIR}/*.jsonnet`; do
   prefix=${file%%.dashboard.py}
+  prefix=${prefix%%.jsonnet}
   echo "Generating: ${prefix}.json"
-  python3 /usr/local/bin/generate-dashboard -o ${prefix}.json ${file}
+  if [[ $file == *.dashboard.py ]] ; then
+      python3 /usr/local/bin/generate-dashboard \
+          -o ${prefix}.json ${file}
+  else
+      jsonnet -J /go/src/github.com/grafana/grafonnet-lib \
+          -o ${prefix}.json ${file}
+  fi
 done
